45bfa2fc91f9d233ab400336f1dee51664629fbe
Marvin <raven.w@foxmail.com>
2022-06-06 20:21:05 +0800

    add platform device to support interrupt.

--- coresight-dump.c
+++ coresight-dump.c
@@ -1,6 +1,7 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <asm/io.h>
+#include <linux/of.h>
 
 #include "coresight-dump.h"
 
@@ -9,12 +10,27 @@ static const struct file_operations coresight_dump_fops = {
         .open = coresight_dump_open,
         .read = coresight_dump_read
 };
-
 static struct miscdevice coresight_dump_dev = {
-        .minor = MISC_DYNAMIC_MINOR,
-        .name = "coresight_dump_dev",
-        .fops = &coresight_dump_fops,
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = "coresight_dump_dev",  /* 指明/dev/目录下的设备节点名 */
+    .fops = &coresight_dump_fops,
 };
+
+static const struct of_device_id coresight_dump_match[] = {
+	{.compatible = "xlnx,DMA-AXI-LiTE-1.0"},
+	{}
+};
+MODULE_DEVICE_TABLE(of, coresight_dump_match);
+static struct platform_driver coresight_dump_driver = {
+        .probe = coresight_dump_probe,
+        .remove = coresight_dump_remove,
+        .driver = {
+                .owner = THIS_MODULE,
+                .name = "coresight_dump",
+                .of_match_table	= coresight_dump_match,	
+        }
+};
+
 
 static void __iomem *axi_data_ptr[AXI_LITE_REG_NUM]; 
 
@@ -23,6 +39,10 @@ static int __init coresight_dump_init(void){
         for(i = 0;i<AXI_LITE_REG_NUM;i++){
                 axi_data_ptr[i] = ioremap(AXI_LITE_BASE + AXI_LITE_WIDTH*i, AXI_LITE_WIDTH);
         }
+	if (platform_driver_register(&coresight_dump_driver)) {
+		pr_info("Error registering platform driver\n");
+		return -1;
+	}
         if (misc_register(&coresight_dump_dev)) {
                 printk("Coresight Dump device register failed...\n");
                 return -1;
@@ -36,14 +56,41 @@ static void __exit coresight_dump_exit(void){
         for(i = 0;i<AXI_LITE_REG_NUM;i++){
                 iounmap(axi_data_ptr[i]);
         }
+        platform_driver_unregister(&coresight_dump_driver);
         misc_deregister(&coresight_dump_dev); 
         printk("CoreSight Dump exit.");
 }
 
+static int coresight_dump_probe(struct platform_device *pdev){
+        int err;
+        pl_ps_irq = platform_get_irq(pdev,0);
+        if (pl_ps_irq <= 0)
+		return -ENXIO;
+        err = request_irq(pl_ps_irq,
+			coresight_dump_irq_handler,
+			IRQF_TRIGGER_RISING | IRQF_ONESHOT,
+			coresight_dump_dev.name, NULL);				   
+	if (err) {
+		printk(KERN_ALERT "irq_probe irq error=%d\n", err);
+	}
+	else
+	{
+		printk("irq = %d gained.\n", pl_ps_irq);
+	}
+        return 0;
+}
+
+static int coresight_dump_remove(struct platform_device *pdev){
+        free_irq(pl_ps_irq, NULL);
+	printk("irq = %d freed.\n", pl_ps_irq);
+ 
+	return 0;
+}
+
 static int coresight_dump_open(struct inode *inode, struct file *filp)  
 {
-    printk("CoreSight Dump dev is open!...\n");
-    return 0;
+        printk("CoreSight Dump dev is open!...\n");
+        return 0;
 }
 
 static ssize_t coresight_dump_read(struct file *file, char __user *buff, size_t count, loff_t *ppos){
@@ -63,10 +110,13 @@ static int coresight_dump_print(void){
         return 0;
 }
 
-// static irqreturn_t coresight_dump_irq_handler(int irq, void *dev_id){
-//         printk();
-//         return IRQ_HANDLED;
-// }
+static irqreturn_t coresight_dump_irq_handler(int irq, void *dev_id){
+        printk("irq = %d\n", irq);
+        coresight_dump_print();
+        return IRQ_HANDLED;
+}
+
+
 
 module_init(coresight_dump_init);
 module_exit(coresight_dump_exit);
--- coresight-dump.h
+++ coresight-dump.h
@@ -1,14 +1,19 @@
 #include <linux/fs.h>
 #include <linux/device.h>
+#include <linux/platform_device.h>
 #include <linux/miscdevice.h>
+#include <linux/interrupt.h>
 
 #define AXI_LITE_WIDTH      4
 #define AXI_LITE_BASE       0xA0000000
 #define AXI_LITE_REG_NUM    16
 
+static unsigned int pl_ps_irq;
 static int __init coresight_dump_init(void);
 static void __exit coresight_dump_exit(void);
+static int coresight_dump_probe(struct platform_device *pdev);
+static int coresight_dump_remove(struct platform_device *pdev);
 static int coresight_dump_open(struct inode *, struct file *);
 static ssize_t coresight_dump_read(struct file *, char __user *, size_t, loff_t *);
 static int coresight_dump_print(void);
-// static irqreturn_t coresight_dump_irq_handler();
+static irqreturn_t coresight_dump_irq_handler(int, void *);
